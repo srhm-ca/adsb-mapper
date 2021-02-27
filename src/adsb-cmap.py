8096a994d84eec91e1092a37b02f31aab92e0725
srhm <sil.hamilton@gmail.com>
2021-02-27 18:07:53 -0500

    Cleaned up the UI - now shows range and flight information.

--- src/adsb-cmap.py
+++ src/adsb-cmap.py
@@ -5,6 +5,7 @@ import argparse
 import curses
 import time
 import sys
+import math
 
 
 def get_arg():
@@ -41,15 +42,51 @@ def start_window():
     """Initialize curses instance and return window"""
     stdscr = curses.initscr()
     curses.cbreak()
+    curses.curs_set(0)
     rows, cols = stdscr.getmaxyx()
     window = curses.newwin(rows, cols, 0, 0)
     return window, stdscr
 
 
-def scale_pos(a, b, s):
+def update_window(state, window, args):
+    """Update the curses window"""
+    window.clear()
+    if curses.is_term_resized:
+        rows, cols = stdscr.getmaxyx()
+        window.resize(rows, cols)
+    write_map(window, state, args.lat, args.lon, args.deg)
+    window.refresh()
+
+
+def conv_linear(a, b, s):
     """Generic function for scaling coordinates"""
     (a1, a2), (b1, b2) = a, b
     return b1 + ((s - a1) * (b2 - b1) / (a2 - a1))
+
+
+def conv_lat(lat):
+    """Convert a given latitude into kilometres"""
+    return abs(lat)*110.574
+
+
+def conv_lon(lat, lon):
+    """Convert a given longitude into kilometres"""
+    return (abs(lon))*(111.320*math.cos(math.radians(lat)))
+
+
+def calc_distance(lat, lon, lat1, lon1):
+    """Calculate the distance between two points in kilometres"""
+    x = abs(conv_lat(lat)-conv_lat(lat1))
+    y = abs(conv_lon(lat, lon) - conv_lon(lat1, lon1))
+    return ((y**2) + (x**2))**0.5
+
+
+def calc_viewbox(lat, lon, deg):
+    """Return approximate viewing range in kilometres given lat/lon co-ords"""
+    viewbox = []
+    viewbox.append(abs((conv_lat(lat - deg) - conv_lat(lat + deg))))
+    viewbox.append(abs(conv_lon(lat, lon - deg) - conv_lon(lat, lon + deg)))
+    return viewbox
 
 
 def check(x, deg):
@@ -62,29 +99,43 @@ def exit_gracefully():
     curses.nocbreak()
     curses.echo()
     curses.endwin()
+    curses.curs_set(1)
     sys.exit()
 
 
 def write_map(window, state, lat, lon, deg):
     """Write to screen an ASCII map based on the latest JSON data"""
     rows, cols = stdscr.getmaxyx()
+    viewbox = calc_viewbox(lat, lon, deg)
     window.addstr(int(rows/2), int(cols/2), "o")
+
+    for i in range(rows-1):
+        if i == int(rows/2):
+            window.addstr(i, 2, str(int(viewbox[0])) + " KM")
+            window.addstr(i, 0, ".")
+        else:
+            window.addstr(i, 0, ".")
+    for i in range(cols-1):
+        if i == int(cols/2):
+            window.addstr(int(rows-2), i, str(int(viewbox[1])) + " KM")
+            window.addstr(int(rows-1), i, ".")
+        else:
+            window.addstr(int(rows-1), i, ".")
+
     for ent in state:
-        x = scale_pos((check(lat, deg)), (-abs(rows), 0), float(ent["lat"]))
-        y = scale_pos((check(lon, deg)), (0, cols), float(ent["lon"]))
+        x = conv_linear((check(lat, deg)), (-abs(rows), 0), float(ent["lat"]))
+        y = conv_linear((check(lon, deg)), (0, cols), float(ent["lon"]))
         window.addstr(abs(int(x)),
                       int(y),
                       "x")
-
-
-def update_window(state, window, args):
-    """Update the curses window"""
-    window.clear()
-    if curses.is_term_resized:
-        rows, cols = stdscr.getmaxyx()
-        window.resize(rows, cols)
-    write_map(window, state, args.lat, args.lon, args.deg)
-    window.refresh()
+        window.addstr(abs(int(x-1)),
+                      int(y),
+                      str(ent["flight"]))
+        window.addstr(abs(int(x-2)),
+                      int(y),
+                      str(int(calc_distance(float(ent["lat"]),
+                                            float(ent["lon"]),
+                                            lat, lon))) + " KM")
 
 
 if __name__ == '__main__':
